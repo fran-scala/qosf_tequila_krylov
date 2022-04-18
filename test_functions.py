b17cb8213153f8488ee69004b077442b2bdf58e2
Francesco Scala <91551202+fran-scala@users.noreply.github.com>
2022-04-18 11:29:12 +0200

    Add test_simple_transition, test_random_transition

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,4 @@
 import tequila as tq
-#from tequila 
 import numpy as np
 import copy
 
@@ -214,7 +213,8 @@ def test_simple_transition():
     '''
     Function that tests if make_transition function is working correctly.
     It creates a simple circuit in order to check that both real and imaginary 
-    part are calculated in the right way. 
+    part of the transition elementare calculated in the right way for a given 
+    Hamiltonian. 
 
     Returns
     -------
@@ -261,14 +261,116 @@ def test_simple_transition():
     
     return
 
+def test_random_transition():
+    '''
+    Function that tests if make_transition function is working correctly.
+    It creates circuits with random number of qubits, random rotations and 
+    random angles and a random Hamiltonian with random number of (random) 
+    Pauli strings.
+
+    Returns
+    -------
+    None.
+
+    '''
+    
+    rotation_gates = ['rx', 'ry', 'rz']
+    
+    #np.random.seed(111)
+    n_qubits = np.random.randint(1, high=5)
+    
+    U = {}
+    for j in range(2):
+        n_rotations = np.random.randint(n_qubits, high=n_qubits*3)
+        gates_list = [np.random.choice(rotation_gates) for i in range(n_rotations)]
+        
+        angles = 2*np.pi * np.random.rand(n_rotations)
+        
+        circ = tq.QCircuit()
+        for i, angle in enumerate(angles):
+            qb = i%n_qubits+1
+            
+            if gates_list[i]=='rx':
+                circ += tq.gates.Rx(angle=angle, target=qb)
+            
+            elif gates_list[i]=='ry':
+                circ += tq.gates.Ry(angle=angle, target=qb)
+                
+            elif gates_list[i]=='rz':
+                circ += tq.gates.Rz(angle=angle, target=qb)
+        U[j] = circ
+    
+    #print(U[0])
+    #print(U[1])
+    
+    #make random hamiltonian
+    paulis = ['X','Y','Z']
+    n_ps = np.random.randint(1, high=2*n_qubits+1)
+    
+    ham = ''
+    for ps in range(n_ps):
+        #"1.0*Y(0)X(1)+0.5*Y(1)Z(0)"
+        coeff = '{}*'.format(round(np.random.sample(),2))
+        pauli_str = ''
+        for qb in range(n_qubits):
+            pauli_str += '{}({})'.format(np.random.choice(paulis), qb)
+        
+        if ps < n_ps-1:
+            pauli_str += '+'
+                
+        ham += coeff+pauli_str
+    
+    #print(ham)
+    
+    H = tq.QubitHamiltonian(ham)
+    
+    trans_el = make_transition(U0=U[0], U1=U[1], H=H)
+    
+    correct_trans_el = 0.0 + 0.0j
+    
+    wfn0 = tq.simulate(U[0])
+    
+    for ps in H.paulistrings:
+        
+        c_k = ps.coeff
+       
+        U_k = p2g(ps,first_qubit=1)
+        wfn1 = tq.simulate(U[1]+U_k)
+        
+        tmp = wfn0.inner(wfn1)
+        correct_trans_el += c_k*tmp
+    
+    assert np.isclose(correct_trans_el, trans_el, atol=1.e-4)
+    
+    return
+
+
+
 #-----------------------------------------------
 # MAIN function
 #-----------------------------------------------
 if __name__ == "__main__":
   
-    #test_simple_overlap()
+    test_simple_overlap()
     
-    #test_simple_transition()
+    test_simple_transition()
     
     test_random_overlap()
-    #tq.draw(dic[0], backend='qiskit')
+    
+    test_random_transition()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
